d6f96ea449456d4c6db1c499e680b2082334736f
Lidia Kovac <lidia.kovac1998@gmail.com>
2023-03-09 12:39:01 +0100

    add lezione

---
+++ js/funzioni.js
@@ -0,0 +1,17 @@
+const sum = (a,b) => {
+    return a+b
+}
+
+sum(4,5)
+
+const valore = "color"
+
+const background = {
+    image: "http://immagini.it/1.png",
+    color: "",
+    position: ""
+}
+
+background[valore] //<<<<
+
+background["color"] //http://imagini.it/1.png
